9f69e986b6d2344fbee442cd0b8d76674e6df914
Vitoria Do Nascimento Galdino <vitoria.galdyno@gmail.com>
2022-04-12 22:17:46 -0300

    Criar variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,64 @@
+// Exercícios de interpretação de código
+
+// 1. Vai mostrar o conteudo que esta na variavel criada, no caso da B sera 10 e da A e B sera 10 e 5.
+
+// 2. Vai mostrar o conteudo que esta na variavel criada A, B e C.
+
+// 3. let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// let valorRecebidoPordia = prompt("Quanto você recebe por dia?")
+
+
+
+//Exercícios de escrita de código
+
+//1- 
+
+prompt ("Meu nome é?")
+
+console.log (prompt ("Meu sobrenome é?") )
+
+
+
+
+const novaVariavel = 80
+
+
+let idade = 23
+console.log( idade, novaVariavel )
+
+idade = 21
+idade = 22
+
+const nome = "Vitória"
+const Idade = 22
+
+console.log("Olá", "Meu nome é", nome, "e eu tenho", Idade, "anos")
+
+//2-
+
+let euEstoudePreto = true
+let euSouHomem = false
+let euSouMulher = true
+
+console.log(euEstoudePreto, euSouHomem, euSouMulher)
+
+
+//3- 
+
+let a = 10
+let b = 25
+let c = 0
+
+console.log("O novo valor de a é", 25)
+console.log("O novo valor de b é", 10)
+console.log("O novo valor de c é", 0)
+
+
+
+
+
+
+
+
+
+

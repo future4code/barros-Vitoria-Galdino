3fc3de8993bb197edb3c8c9ea19f0291fca1bb8b
Vitoria Do Nascimento Galdino <vitoria.galdyno@gmail.com>
2022-05-12 20:08:37 -0300

    Exercicios-feitos-ate-o-11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -20,13 +20,28 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-   
+    const numeroPar = function(numero) {
+        return (numero % 2 === 0) 
+    }
+
+    const pares = array.filter(numeroPar)
+    return pares
 
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+     let numeroPar = array.filter((numero) => {
+     return(numero % 2 === 0)
+     })
+    
+    let numeroElevado = numeroPar.map((numero) => {
+      return(Math.pow(numero, 2))
+
+     })
+
+     return numeroElevado
+      
 }
 
 // EXERCÍCIO 06
@@ -49,22 +64,56 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+ 
+ let numeroMaior
+ let diferente
+ let divisivel
+
+ if (num1 > num2) {
+     numeroMaior = num1
+     divisivel = num1 % num2 === 0
+     diferente = num1 - num2
+}else{
+     numeroMaior = num2
+     divisivel = num2 % num1 === 0
+     diferente = num2 - num1
+ }
+ objetoDoisNumeros = {
+    maiorNumero: numeroMaior,
+    maiorDivisivelPorMenor: divisivel,
+    diferenca: diferente
+ }
+
+ return objetoDoisNumeros
 
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let numeroPar = []
+   for(let i = 0; numeroPar.length < n; i++) {
+       if (i % 2 === 0) {
+             numeroPar.push(i)
+       }
+   }
+   return numeroPar
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
+ if(ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {
+    return "Escaleno"
+ }else if (ladoA === ladoB && ladoB === ladoC && ladoA === ladoC) {
+    return "Equilátero"
+ }else{
+     return "Isósceles"
+ }
 
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    
 }
 
 // EXERCÍCIO 11

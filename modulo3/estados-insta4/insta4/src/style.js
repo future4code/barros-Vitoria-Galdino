fb78e904651ba97e4f23c0a885f1c1a89ecb68d2
Vitoria Do Nascimento Galdino <vitoria.galdyno@gmail.com>
2022-07-07 21:05:54 -0300

    Exercicio-Renderizacao-Parte2

--- modulo3/estados-insta4/insta4/src/style.js
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -4,6 +4,19 @@ export const Container=styled.div`
     display: flex;
     justify-content: center;
     flex-direction: column;
-    align-items: center
- 
-`
+    align-items: center;
+ `
+
+ export const MyForm=styled.form`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  input{
+    margin: 0px 3px;
+    font-size: 1.2vw;
+  }
+  button{
+    font-size: 1.2vw;
+  }
+ `
